07f58684255f6efa78846461a32853b2650787bf
Gerrit Jansen van Vuuren <gvanvuuren@Gerrits-MBP.home>
2023-07-19 12:54:24 +0200

    more commits

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::fs::File;
-use std::io;
+use std::{fs, io};
 use std::io::{Read, Write};
-
+use chrono::Utc;
 
 struct Context {
+    file_size_limit: u64,
+    file_count_limit: u32,
+
+    current_file_path: String,
     current_file: File,
 }
 
@@ -22,7 +26,14 @@ fn main() -> () {
         match handle.read(&mut buffer) {
             Ok(bytes_read) => {
                 if bytes_read > 0 {
-                    handle_std_bytes(&mut context, &buffer, bytes_read).expect("Cannot write to file");
+                    let context2 = &mut context;
+                    handle_std_bytes(context2, &buffer, bytes_read).expect("Cannot write to file");
+                    match check_and_rotate(context2).unwrap() {
+                        None => {}
+                        Some(context_new) => {
+                            context = context_new;
+                        }
+                    }
                 }
             }
             Err(err) => {
@@ -42,8 +53,46 @@ fn main() -> () {
     println!("Hello, world!");
 }
 
+fn check_and_rotate(context: &mut Context) -> Result<Option<Context>, std::io::Error> {
+    let file_metadata = context.current_file.metadata().unwrap();
+    let file_size = file_metadata.len();
+
+    if file_size > context.file_size_limit {
+        // rotate
+        println!("Rotatefile: file_size {} ", file_size);
+
+        let roll_file_name = roll_file_name_from(&context.current_file_path);
+
+        fs::rename(&(context.current_file_path), &roll_file_name)?;
+
+        let file = File::create(&(context.current_file_path))?;
+        let context2 = Context {
+            file_size_limit: context.file_size_limit,
+            file_count_limit: context.file_count_limit,
+            current_file_path: String::from(context.current_file_path.as_str()),
+            current_file: file,
+        };
+        return Ok(Some(context2));
+    }
+
+
+    return Ok(None);
+}
+
+fn roll_file_name_from(path: &String) -> String {
+    let (name, extension) = match path.rfind('.') {
+        Some(index) => path.split_at(index),
+        None => (path.as_str(), "")
+    };
+
+    let timestamp = Utc::now().format("%Y-%m-%d_%H-%M-%S").to_string();
+    let roll_file_name = format!("{}_{}{}", name, timestamp, extension);
+
+    return roll_file_name;
+}
+
 fn create_context(file_path: String) -> io::Result<Context> {
-    let current_file_res = File::create(file_path);
+    let current_file_res = File::create(file_path.clone());
 
     match current_file_res {
         Ok(_) => {
@@ -57,7 +106,10 @@ fn create_context(file_path: String) -> io::Result<Context> {
     let current_file = current_file_res.unwrap();
 
     return Ok(Context {
+        current_file_path: file_path.clone(),
         current_file,
+        file_size_limit: 1024 * 1024 * 128,
+        file_count_limit: 3,
     });
 }
 
